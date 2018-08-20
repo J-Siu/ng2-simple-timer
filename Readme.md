b2b93e37d9016f07202d54e81668bde213da155b
John Siu <js@iMacPro.local>
2018-08-20 04:11:12 -0400

    6.0.0

--- Readme.md
+++ Readme.md
@@ -22,6 +22,12 @@ Name/ID(string) base API. RxJS object not exposed.
 - [Changelog](#changelog)
 - [License](#license)
 
+## Version
+
+- For Angular 2.x.x, please use 1.3.1.
+- For Angular 4.x.x, please use 1.3.5.
+- For Angular 6.x.x, please use 6.0.0.
+
 ## Install
 
 ```
@@ -205,12 +211,15 @@ Plunker: [Angular2 Simple Timer Example](http://embed.plnkr.co/HaTd8q/)
 	- Add `delay` option for `newTimer`
 * 1.3.5
 	- Bugfix for `newTimer`
+* 6.0.0
+	- Update for Angular 6.x, which include moving from RxJS 5.1 to 6.0
+	- Update version to match major version of Angular
 
 ## License
 
 The MIT License
 
-Copyright (c) 2017
+Copyright (c) 2018
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
--- src/simple-timer.ts
+++ src/simple-timer.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
-import { Observable, Subscription } from 'rxjs/Rx';
+import { Observable, Subscription, timer } from 'rxjs';
 import { UUID } from 'angular2-uuid';
 
 interface TimerList {
 	[name: string]: {
 		second: number,
-		observable: Observable<any>
+		t: Observable<any>
 	};
 }
 
@@ -16,34 +16,34 @@ interface SubscriptionList {
 	};
 }
 
-@Injectable()
+@Injectable({ providedIn: 'root' })
 export class SimpleTimer {
 
-	private timer: TimerList = {};
+	private timers: TimerList = {};
 	private subscription: SubscriptionList = {};
 
 	getTimer(): string[] {
-		return Object.keys(this.timer);
+		return Object.keys(this.timers);
 	}
 	getSubscription(): string[] {
 		return Object.keys(this.subscription);
 	}
 	newTimer(name: string, sec: number, delay: boolean = false): boolean {
-		if (name === undefined || sec === undefined || this.timer[name]) {
+		if (name === undefined || sec === undefined || this.timers[name]) {
 			return false;
 		}
-		let o
+		let t: Observable<any>
 		if (delay) {
-			o = Observable.timer(sec * 1000, sec * 1000);
+			t = timer(sec * 1000, sec * 1000);
 		}
 		else {
-			o = Observable.timer(0, sec * 1000);
+			t = timer(0, sec * 1000);
 		}
-		this.timer[name] = { second: sec, observable: o };
+		this.timers[name] = { second: sec, t: t };
 		return true;
 	}
 	delTimer(name: string): boolean {
-		if (name === undefined || !this.timer[name]) {
+		if (name === undefined || !this.timers[name]) {
 			return false;
 		}
 		let s = this.getSubscription();
@@ -54,8 +54,8 @@ export class SimpleTimer {
 			}
 		});
 		// delete queue 'name' subject and observable
-		delete this.timer[name].observable;
-		delete this.timer[name];
+		delete this.timers[name].t;
+		delete this.timers[name];
 	}
 	/**
 	 *
@@ -63,13 +63,13 @@ export class SimpleTimer {
 	 * @param callback
 	 */
 	subscribe(name: string, callback: () => void): string {
-		if (!this.timer[name]) {
+		if (!this.timers[name]) {
 			return '';
 		}
 		let id = name + '-' + UUID.UUID();
 		this.subscription[id] = {
 			name: name,
-			subscription: this.timer[name].observable.subscribe(callback)
+			subscription: this.timers[name].t.subscribe(callback)
 		}
 		return id;
 	}
